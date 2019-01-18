c12af584177e413a618af2ba62f704efbdd30414
Raine Wallace <rainebeaudubs@gmail.com>
2019-01-18 15:47:01 -0500

    (after Sprint time) Converted most everything to styled components, but have a couple questions about ones I wasn't sure how to convert.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+const CardDiv = styled.div `
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;`
+
+const CardHeadline = styled.div `
+  font-size: 25px;
+  font-family: Didot, serif;`
+
+const CardAuthor = styled.div `
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+    img {
+      width: 40px;
+    }
+    span {
+      padding-left: 10px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;`
+
+const CardImgCont = styled.div `
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+}`
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <CardDiv>
+      <CardHeadline>{props.card.headline}</CardHeadline>
+      <CardAuthor>
+        <CardImgCont>
           <img src={props.card.img} alt="headline" />
-        </div>
+        </CardImgCont>
         <span>By {props.card.author}</span>
-      </div>
-    </div>
+      </CardAuthor>
+    </CardDiv>
   );
 };
 
