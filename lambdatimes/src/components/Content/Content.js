41add08849099c78c0a9d2be5da1ef93bef75cac
Raine Wallace <rainebeaudubs@gmail.com>
2019-01-25 12:03:18 -0500

    Tabs are rendering to the screen.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,10 +17,17 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
     // Once the component has mounted, get the data and reflect that data on the state.
   }
 
   changeSelected = tab => {
+    this.setState({
+      selected: tab
+    })
     // this function should take in the tab and update the state with the new tab.
   };
 
